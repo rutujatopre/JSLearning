a515ba2d712516b6733395854f23d788594afbc0
rutujatopre <rutujamm27@gmail.com>
2023-02-16 15:55:26 +0530

    variable_declaration.js
    variable introduction program

---
+++ variableAssingment.js
@@ -0,0 +1,21 @@
+console.log("---------- First Step ----------")
+var myFullName;
+console.log(myFullName);
+myFullName="Rutuja Topre";
+console.log("My full Name is:", myFullName);
+myFullName="Rutuja Bhushan Topre";
+console.log("My Full Name is:", myFullName);
+console.log("---------- Second Step ----------");
+var myLove="I Love Only JS";
+console.log(myLove);
+console.log("---------- Third Step ----------");
+var firstName="Rutuja";
+var lastName="Topre";
+var age="32";
+var address="Arvi, Dist.Wardha";
+var pincode="445206";
+var gender="Female";
+var isMarried="Yes";
+console.log("My First name:",firstName,"*Last name:",lastName, 
+"*My age:",age, "*Address:",address , "*Pincode:",pincode,
+"*Gender:", gender,"*Marrital status:",isMarried)
