2d4c242cd083575e4af5a7e5d118d54a2743b143
rutujatopre <rutujamm27@gmail.com>
2023-02-16 15:56:58 +0530

    updated hello with variable

--- helloWord.js
+++ helloWord.js
@@ -1,1 +1,19 @@
 console.log("Rutuja Topre");
+console.log("Pune");
+
+var fullName="RutujaTopre";//veriable intialization
+console.log(fullName);
+var city;//variable delaration
+console.log(city);//default value of the variable is undefined
+city="Amravati"; //variable intilization
+console.log(city);
+city="Nagpur";
+console.log(city);
+
+var age="31";// variable declaration and intilization in same line
+age="32";
+age="33"
+console.log(age);
+
+var name="Rutuja";
+console.log("My Name is", name);//output with heading
