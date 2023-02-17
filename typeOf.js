c8656f2ee38e2577dc89879afb0f36d77590b8b5
rutujatopre <rutujamm27@gmail.com>
2023-02-17 16:21:34 +0530

    TypeOfAssing

---
+++ typeOf.js
@@ -0,0 +1,32 @@
+console.log(" ----------- first ----------");
+var bankName;
+console.log("Bank Name:",bankName , ",Type:",typeof bankName);
+bankName ="SBI Bank";
+console.log("Bank name:",bankName, ",Type:",typeof bankName);
+
+console.log(" ----------- Second ----------");
+var marks ="90%";
+console.log("Marks:",marks,",Type:",typeof marks);
+
+console.log(" ----------- Third ----------");
+var isWorking;
+console.log("Is Working:",isWorking);
+isWorking = true;
+console.log("Is Working:",isWorking);
+console.log("Type of isWorking is:", typeof isWorking);
+
+console.log(" ----------- Fourth ----------");
+var totalCount ="Hundred and Seven";
+console.log("Total Count:",totalCount,",Type:",typeof totalCount);
+totalCount=107;
+console.log("Total Count:",totalCount,",Type:",typeof totalCount);
+
+
+
+
+
+
+
+
+
+
