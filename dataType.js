bf0dae90936451ee82ab49c2b1dfe850bda9121e
rutujatopre <rutujamm27@gmail.com>
2023-02-17 16:22:15 +0530

    datatypePractice.js

---
+++ dataType.js
@@ -0,0 +1,27 @@
+var myName="Virat Kohli";
+var fatherName="Kishor Kohli";
+
+var age= 31;
+var weight= 82.5;
+
+var isMarried= true;
+var isLazy= false;
+
+var typeOffMyName = typeof myName;
+console.log("Typeof variable myName ;", typeOffMyName);
+console.log("Typeof variable fatherName:", typeof fatherName);
+
+console.log("Typeof age is:", typeof age);
+console.log("Typeof weight is:", typeof weight);
+
+console.log("Typeof variable isMarried is:",typeof isMarried);
+console.log("Typeof variable isLazy is:", typeof isLazy)
+
+var gratuation;
+console.log("Typeof gratuation variable is:",typeof  gratuation);
+console.log("value of gatution is:", gratuation);
+
+var city= null;
+console.log("Typeof variable city is:", typeof city);
+console.log("Value of variable city is:", city);
+
