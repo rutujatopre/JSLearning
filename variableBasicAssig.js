bad0abaada9fe77f96a40924205afdae8e8f479a
rutujatopre <rutujamm27@gmail.com>
2023-02-17 16:01:22 +0530

    variableBasicAssig.js
    string, number and boolean

---
+++ variableBasicAssig.js
@@ -0,0 +1,28 @@
+var rollNo = 10;
+var phNumber = 9890370804;
+var percentage = 97;
+console.log("Roll Number:",rollNo ,"Type:", typeof rollNo);
+console.log("Phone Number:",phNumber,"Type:", typeof phNumber);
+console.log("Percentage:", percentage,"Type:", typeof percentage);
+console.log("------------------------------");
+
+var book="Mrutunjay", picture="Supper 30" , song =" Wande Matram";
+console.log("Fav Book:", book , "Type:", typeof book);
+console.log("Fav Picture:",picture, "Type:", typeof picture);
+console.log("Fav Song:", song, "Type:", typeof song);
+console.log("-------------------------------");
+
+var isCrezy= false , isMarried = true, isLazy= false;
+console.log("Is Crezy:",isCrezy, "Type:",typeof isCrezy);
+console.log("Is Married:",isMarried, "Type:", typeof isMarried);
+console.log("Is Lazy:", isLazy,"Type:", typeof isLazy);
+
+console.log("-------Difference between Null and Undefined-------");
+var gratuation;
+console.log("Typeof gratuation variable is:",typeof  gratuation);
+console.log("value of gatution is:", gratuation);
+
+var city= null;
+console.log("Typeof variable city is:", typeof city);
+console.log("Value of variable city is:", city);
+console.log("Type of null is Object and Undefined is undefined.");
