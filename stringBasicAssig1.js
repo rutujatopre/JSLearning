731c510473076d53e67746941296edc4fcd9b0c1
rutujatopre <rutujamm27@gmail.com>
2023-02-21 18:45:26 +0530

    string.js

---
+++ stringBasicAssig1.js
@@ -0,0 +1,15 @@
+
+function stringBasic(){
+    console.log("My Dream company is: Microsoft");
+}
+stringBasic();
+console.log("----------------------------------")
+
+var hobby1 = "Reading News Paper,  "; 
+var hobby2 = "Painting,  ";
+var hobby3 = "Jogging";
+console.log(" My hobbies are:", hobby1,hobby2,hobby3);
+
+var sum = hobby1.length + hobby2.length + hobby3.length;
+console.log(" Sum of characture of all three hobbies:",sum);
+
