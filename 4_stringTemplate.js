19d832d199cdac363e5db48a2bea5ec09deb6eaa
rutujatopre <rutujamm27@gmail.com>
2023-02-22 15:47:09 +0530

    string template

---
+++ 4_stringTemplate.js
@@ -0,0 +1,8 @@
+console.log('You are"Champ" man');
+console.log(`You are"Champ" man`);
+
+var firstName = "Mohit";
+var lastName = "Sharma";
+var city ="Pune";
+console.log("My first name:",firstName ,"Last name:",lastName,"City:",city);//normal console
+console.log(`My first name:${firstName} Last name:${lastName} City:${city}`);//string template
