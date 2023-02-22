e5a35a8879d7b8aff98d22eb5a8e50d581c1f4a3
rutujatopre <rutujamm27@gmail.com>
2023-02-22 15:47:48 +0530

    Introduction to operater

---
+++ 05_operator.js
@@ -0,0 +1,54 @@
+console.log(`---------- Arithmatic operator ----------`);
+var num1 = 10;
+var num2 = 2;
+var result =num1 + num2;//Addition
+console.log(`Addition is ${result}`);
+
+var result =num1 - num2;//Subtraction
+console.log(`Subtraction is ${result}`);
+
+var result =num1 * num2;//Multiplication
+console.log(`Multiplication is ${result}`);
+
+var result =num1 / num2;//Division
+console.log(`Division is ${result}`);
+
+var result =num1 % num2;//Remainder or modulus
+console.log(`Remainder is ${result}`);
+
+
+var result = 5 ** 3;//Exponential(cube)
+console.log(`Cube of 5 is ${result}`);
+
+var num =10;//compound addition
+num += 20 ;// num = num +20
+console.log(`Compound Addion += is ${num}`);
+
+console.log(`---------- Comapring operator ----------`);
+var num3 = '10';
+var num4 = 10;
+console.log(num3 == num4);//converting string into number
+console.log(num3 === num4);//keeping as it is string as string and number as number
+
+console.log(num3 != num4);//not equal
+
+console.log(10 > 20);//greater than
+
+console.log(10 < 20);//less than
+
+console.log(10 >= 20);//greater than or equal to
+
+console.log(10 <= 20);//less than or equal to
+
+
+
+
+
+
+
+
+
+
+
+
+
