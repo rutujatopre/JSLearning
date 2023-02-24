4ee6f15f07cca20e9ac1b666ed33570149c5ee6c
rutujatopre <rutujamm27@gmail.com>
2023-02-24 15:50:24 +0530

    Intro to NAN

---
+++ 05_nan.js
@@ -0,0 +1,17 @@
+console.log(10 + "Hi");
+console.log('Good' + ' Morning');
+
+console.log("Operation: 10-Hi ==>", 10-"Hi");//NaN = not a number
+
+console.log("Operation: 0/0 ==>", 0/0);
+
+var num = "10";
+console.log(typeof num);
+
+var num2 = +num; //"10" ==> 10
+console.log(`${num2} and its type ${typeof num2}`);
+
+var myName = "Robert";
+var result = +myName;
+console.log(`${result} and its type ${typeof result}`);
+
