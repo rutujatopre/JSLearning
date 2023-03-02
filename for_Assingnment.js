80d3f362405403bf090e5245f90bdb49f9156d03
abhu8790 <abhu8790@gmail.com>
2023-03-02 15:53:59 +0530

    forloop assingnment
    basic program

---
+++ for_Assingnment.js
@@ -0,0 +1,40 @@
+
+console.log(`1. Numbers from 5 to 15`);
+for (let index = 5; index <=15; index++)
+{
+    console.log(index);
+}
+console.log(`2. Numbers from 50 to 40`);
+for (let index = 50; index >=40; index--)
+{
+    console.log(index);
+}
+console.log(`3. First 15 odd numbers`);
+for (let index = 1; index < 30; index= index +2)
+{
+    console.log(index);
+}
+
+console.log(`4. First 10 even numbers`);
+for (let index = 2; index <=20; index =index+2)
+{
+    console.log(index);
+}
+
+
+
+console.log(`5. table of 5`);
+for (let index = 5; index<=50; index= index+5)
+{
+    console.log(index);
+}
+console.log(`6. table of 10`);
+for (let index = 10; index<=100; index= index+10)
+{
+    console.log(index);
+}
+console.log(`7. table of 10 in reverse order`);
+for (let index = 100; index>=10; index= index-10)
+{
+    console.log(index);
+}
