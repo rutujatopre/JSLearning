f9517ddee78a66d176fa6023006bdcf6e30a5572
abhu8790 <abhu8790@gmail.com>
2023-03-04 16:36:05 +0530

    factorialAssignment

--- factorial_B.js
+++ factorial_B.js
@@ -1,58 +1,29 @@
-/*function factorialOfWordsCount(string)
+function factorialOfWordsCount(string)
 {
- let word = string.split(" ");
- let length = word.length;
- if(word.length == 0)
+ 
+ if(string==null||string==undefined || (isNaN(string) && typeof string=="number"))
  {
-   console.log(`Invalid data, please entre valid data`);
+   console.log(`Given string ${string} is invalid, please entre valid data`);
    return;
  }
  else
  {
+   
    let fact = 1;
+   let word = string.split(" ");
+   let length = word.length;
     for (let i = 1; i <= word.length; i++) 
     {
         fact =fact * i;
     }
-    console.log(`Word count of string is ${word.length} . The factorial of words count is ${fact}.`);
-  }
-}
-//factorialOfWordsCount("Revision is the mother of sucess");
-//factorialOfWordsCount("We never fail, we always learn");
-factorialOfWordsCount(null);
-//factorialOfWordsCount("Rutuja Bhushan Topre");
-*/
-function factorialOfWordsCount(string)
-{
-  if(string == null)
-  {
-    console.log(`Passed value is null, Please input valid value`);
-  }
-  else if (string <= 0)
-  {
-    console.log(`Passed value is invalid, please entre valid value`);
-  }
-  else 
-  {
-    let word = string.split(" ");
-    let length = word.length;
-    if(word.length < 0)
-    {
-         console.log(`Invalid data, please entre valid data`);
-    }
-    else
-    {
-        let fact = 1;
-        for (let i = 1; i <= word.length; i++) 
-        {
-            fact =fact * i;
-        }
-        console.log(`Word count of string is ${word.length} . The factorial of words count is ${fact}.`);
-    }
+    console.log(`Given string is ${string}`);
+    console.log(`Word count of given string is ${word.length} . The factorial of words count is ${fact}.`);
   }
 }
 factorialOfWordsCount("Revision is the mother of sucess");
-factorialOfWordsCount('')
-factorialOfWordsCount("We never fail, we always learn");
-factorialOfWordsCount("Rutuja Bhushan Topre");
 factorialOfWordsCount(null);
+factorialOfWordsCount("");
+factorialOfWordsCount("Rutuja Bhushan Topre");
+factorialOfWordsCount("We never fail, we always learn");
+
+
